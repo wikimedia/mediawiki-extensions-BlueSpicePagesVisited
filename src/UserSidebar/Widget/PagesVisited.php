f5ff4d11dfd60bc75dc84f7e197b0c63b5e857f9
pwirth <wirth@hallowelt.biz>
2021-10-23 15:16:33 +0200

    Personal mega menu integration in BlueSpiceDiscovery skin
    Unfortunately the curretn widgets are hard coded to be
    BlueSpice\Calumma\IPanel and therefor we need a new registry
    
    ERM:25779
    [4.1]
    
    Change-Id: Ie9fe610dbaa877f7c8acc3338125e55011e9e18c

---
+++ src/UserSidebar/Widget/PagesVisited.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace BlueSpice\PagesVisited\UserSidebar\Widget;
+
+use BlueSpice\Data\FieldType;
+use BlueSpice\Data\Filter;
+use BlueSpice\Data\Filter\ListValue;
+use BlueSpice\Data\Filter\Numeric;
+use BlueSpice\Data\Filter\StringValue;
+use BlueSpice\Data\ReaderParams;
+use BlueSpice\PagesVisited\Data\Store;
+use BlueSpice\UserSidebar\Widget;
+use BlueSpice\WhoIsOnline\Data\Record;
+use BsStringHelper;
+use Message;
+use Title;
+
+class PagesVisited extends Widget {
+	public const PARAM_TITLE_MAX_LENGHT = 'maxtitlelength';
+	public const PARAM_NAMESPACES = 'namespaces';
+	public const PARAM_COUNT = 'count';
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function shouldRender(): bool {
+		return $this->context->getUser()->isAnon();
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getHeaderMessage(): Message {
+		return $this->context->msg( 'bs-pagesvisited-widget-title' );
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getLinks(): array {
+		if ( $this->context->getUser()->isAnon() ) {
+			return [];
+		}
+		$readerParams = new ReaderParams( $this->makeReaderParams() );
+		$recordSet = ( new Store() )->getReader()->read( $readerParams );
+
+		// Dumb default
+		$maxTitleLength = 20;
+		if ( isset( $this->params[static::PARAM_TITLE_MAX_LENGHT] ) ) {
+			$maxTitleLength = (int)$this->params[static::PARAM_TITLE_MAX_LENGHT];
+		}
+
+		$links = [];
+		foreach ( $recordSet->getRecords() as $record ) {
+			$title = Title::makeTitle(
+				$record->get( Record::PAGE_NAMESPACE ),
+				$record->get( Record::PAGE_TITLE )
+			);
+			if ( !$title ) {
+				continue;
+			}
+			$display = BsStringHelper::shorten( $title->getPrefixedText(), [
+				'max-length' => $maxTitleLength,
+				'position' => 'middle'
+			] );
+			$link = [
+				'href' => $title->getFullURL(),
+				'text' => $display,
+				'title' => $title->getPrefixedText(),
+				'classes' => ' bs-usersidebar-internal '
+			];
+			$links[] = $link;
+		}
+		return $links;
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	protected function makeReaderParams() {
+		$params = [
+			ReaderParams::PARAM_LIMIT => 7,
+			ReaderParams::PARAM_FILTER => [],
+			ReaderParams::PARAM_FILTER => [ [
+				Filter::KEY_COMPARISON => StringValue::COMPARISON_EQUALS,
+				Filter::KEY_PROPERTY => Record::ACTION,
+				Filter::KEY_VALUE => 'view',
+				Filter::KEY_TYPE => FieldType::STRING
+			], [ Filter::KEY_COMPARISON => Numeric::COMPARISON_EQUALS,
+				Filter::KEY_PROPERTY => Record::USER_ID,
+				Filter::KEY_VALUE => (int)$this->context->getUser()->getId(),
+				Filter::KEY_TYPE => 'numeric'
+			] ]
+		];
+		if ( !empty( $this->params[static::PARAM_NAMESPACES] ) ) {
+			$params[ReaderParams::PARAM_FILTER][] = [
+				Filter::KEY_COMPARISON => ListValue::COMPARISON_EQUALS,
+				Filter::KEY_PROPERTY => Record::PAGE_NAMESPACE,
+				Filter::KEY_VALUE => static::PARAM_NAMESPACES,
+				Filter::KEY_TYPE => FieldType::LISTVALUE
+			];
+		}
+		if ( !empty( $this->params[static::PARAM_COUNT] ) ) {
+			$params[ReaderParams::PARAM_LIMIT] = $this->params[static::PARAM_COUNT];
+		}
+
+		return $params;
+	}
+
+}

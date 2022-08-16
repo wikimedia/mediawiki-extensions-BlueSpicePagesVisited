7e13b30a50fba8148f153126461f851266eba05f
miriam schlindwein <schlindwein@hallowelt.com>
2022-08-16 09:46:54 +0200

    Add pages visited droplet
    ERM29396
    
    [4.x]
    
    Change-Id: I8ab82a93b35feb62ac445a01f3ce27dacf6324e1
    (cherry picked from commit 79543762c80ee689f925c8b21510a0c3c9717b90)

---
+++ src/ContentDroplets/VisitedDroplet.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace BlueSpice\PagesVisited\ContentDroplets;
+
+use MediaWiki\Extension\ContentDroplets\Droplet\TagDroplet;
+use Message;
+use RawMessage;
+
+class VisitedDroplet extends TagDroplet {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getName(): Message {
+		return new RawMessage( 'Pages Visited' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getDescription(): Message {
+		return new RawMessage( "Pages visited description" );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIcon(): string {
+		return '';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRLModule(): string {
+		return 'ext.bluespice.pagesvisited.visualEditorTagDefinition';
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getCategories(): array {
+		return [ 'lists', 'visualization', 'data' ];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	protected function getTagName(): string {
+		return 'bs:pagesvisited';
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getAttributes(): array {
+		return [
+			'count' => 7,
+			'maxtitlelength' => 40,
+			'order' => [
+				[
+					[ 'data' ] => 'time',
+					[ 'label' ] => Message::newFromKey( 'bs-pagesvisited-tag-pagesvisited-attr-order-option-time' )
+				],
+				[
+					[ 'data' ] => 'pagename',
+					[ 'label' ] => Message::newFromKey( 'bs-pagesvisited-tag-pagesvisited-attr-order-option-pagename' )
+				]
+			],
+			'namespaces' => ''
+		];
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function hasContent(): bool {
+		return false;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getVeCommand(): ?string {
+		return 'pagesvisitedCommand';
+	}
+
+}
